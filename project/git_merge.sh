a5f196217f59a668d6acc398d451290ab8697570
Zach Musacchio <Musacchio-Davis_Zachary@example.com>
2020-01-21 15:44:29 -0500

    added some concourse stuff randomly LOL

--- project/git_merge.sh
+++ project/git_merge.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-branch=$(git show :/^Merge | egrep -i merge | grep -i / | sed 's/.*\///')
-last_merge=$(git show :/^Merge | grep commit | sed 's/commit //')
-last_commit=$(git show | grep commit | sed 's/commit //')
+branch=$(cd git-repo-master/project && git show :/^Merge | egrep -i merge | grep -i / | sed 's/.*\///')
+last_merge=$(cd git-repo-master/project && git show :/^Merge | grep commit | sed 's/commit //')
+last_commit=$(cd git-repo-master/project && git show | grep commit | sed 's/commit //')
 
 if [ "$last_merge" == "$last_commit" ]; then
    echo "$branch merged to master"
