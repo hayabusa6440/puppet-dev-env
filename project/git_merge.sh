8f79d35dbbb17305b1219e4f4fc8e8d6cb91a176
Zach Musacchio <Musacchio-Davis_Zachary@example.com>
2020-01-22 09:33:55 -0500

    things

--- project/git_merge.sh
+++ project/git_merge.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 
-directory=$(git-repo-master/project) 
-branch=$(cd $directory && git show :/^Merge | egrep -i merge | grep -i / | sed 's/.*\///')
-last_merge=$(cd $directory && git show :/^Merge | grep commit | sed 's/commit //')
-last_commit=$(cd $directory && git show | grep commit | sed 's/commit //')
+branch=$(git show :/^Merge | egrep -i merge | grep -i / | sed 's/.*\///')
+last_merge=$(git show :/^Merge | grep commit | sed 's/commit //')
+last_commit=$(git show | grep commit | sed 's/commit //')
 
 if [ "$last_merge" == "$last_commit" ]; then
    echo "$branch merged to master"
